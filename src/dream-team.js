a2dcc6f5b9b4b6f76106b060d5c2e5769621a56d
Maksim Antonov <33826709+BoL4oNoK@users.noreply.github.com>
2020-02-18 13:11:20 +0300

    fest: Added solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  res = false;
+  if (Array.isArray(members)) {
+    if (members.length == 0) throw 'Not implemented';
+    var arrTeam = [];
+    members.sort().forEach(element => {
+      if ( (typeof element == 'string') && ( /^[a-z _]+$/i.test(element) ) ) {
+        element = element.replace(/^( )+/, "").substr(0,1);
+        arrTeam.push(element.toUpperCase());
+      }
+    });
+    res = arrTeam.sort().join('');
+  }
+  
+  return res;
 };
