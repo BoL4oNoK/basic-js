3025ab47806c25671d51a0efc29d6d6d29a08f83
Maksim Antonov <33826709+BoL4oNoK@users.noreply.github.com>
2020-02-18 11:11:37 +0300

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  var cats = 0;
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+      if (/^\^\^$/g.test(matrix[i][j])) { cats += 1; }
+    }
+  }
+
+  return cats;
 };
